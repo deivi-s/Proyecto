55371492afb7473fdac5c3f5697abc491a855b3e
d-arocutipa <arocutipa17@gmail.com>
2021-12-16 19:36:53 -0500

    Reactive Form Agregado

--- src/app/form-login/form-login.component.ts
+++ src/app/form-login/form-login.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { LoginModel } from '../../models/login.model';
 
 @Component({
   selector: 'form-login',
   templateUrl: './form-login.component.html',
-  styleUrls: ['./form-login.component.css']
+  styleUrls: [ './form-login.component.css' ]
 })
 export class FormLoginComponent implements OnInit {
+
+
+  public objUsuario: LoginModel = new LoginModel();
+
+  model: any = {};
 
   constructor() { }
 
   ngOnInit(): void {
+    this.inicioFormGroup();
+  }
+
+  inicioFormGroup() {
+    this.model = new FormGroup({
+      usuario: new FormControl('', [ Validators.required, Validators.minLength(3) ]),
+      clave: new FormControl('', [ Validators.required, Validators.minLength(3) ]),
+    });
+  }
+
+  get f() {
+    return this.model.controls;
+  }
+
+  onSubmit() {
   }
 
 }
